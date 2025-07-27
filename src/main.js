61706ec5ee5eda386698c4a224edf164295e76cc
Bigwin01 <jiasheng0212@163.com>
2025-07-27 15:35:26 +0800

    update table

--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import VxeUI from 'vxe-pc-ui'
+import 'vxe-pc-ui/es/style.css'
 
-createApp(App).mount('#app')
+import VxeUITable from 'vxe-table'
+import 'vxe-table/es/style.css'
+
+
+createApp(App).use(VxeUI).use(VxeUITable).mount('#app')
